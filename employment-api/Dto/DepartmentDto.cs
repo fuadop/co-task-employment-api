f81bd7c000c432e3fe6ad6addae8508e812ad513
fuadop <fuadolatunji@gmail.com>
2023-08-06 21:02:13 +0100

    :recycle: refactor(validation): validate json input using DTO attribute annotations

--- employment-api/Dto/DepartmentDto.cs
+++ employment-api/Dto/DepartmentDto.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace employment_api.Dto
 {
 	public class DepartmentDto
 	{
-        public string Code { get; set; }
-        public string Name { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(8)]
+        [RegularExpression(@"[A-Z]+")]
+        public string Code { get; set; } = "";
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-zA-Z-\s]+")]
+        [DefaultValue("Technology Department")]
+        public string Name { get; set; } = "";
 	}
 
     public class DepartmentUpdateDto
     {
+        [MinLength(2)]
+        [MaxLength(8)]
+        [RegularExpression(@"[A-Z]+")]
         public string? Code { get; set; }
+
+        [MinLength(2)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-zA-Z-\s]+")]
+        [DefaultValue("Technology Department")]
         public string? Name { get; set; }
     }
 }
--- employment-api/Dto/EmployeeDto.cs
+++ employment-api/Dto/EmployeeDto.cs
@@ -1,21 +1,67 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace employment_api.Dto
 {
 	public class EmployeeDto
 	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string DOB { get; set; }
-		public string PhoneNumber { get; set; }
-		public string DepartmentCode { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(20)]
+        [RegularExpression("[a-zA-Z-]+")]
+        [DefaultValue("John")]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(20)]
+        [RegularExpression("[a-zA-Z-]+")]
+        [DefaultValue("Doe")]
+        public string LastName { get; set; } = "";
+
+        [Required]
+        [RegularExpression(@"^([1-3]?\d{1}\/[0-1]?\d{1}\/\d{4}){1}")]
+        [DefaultValue("16/10/2005")]
+        public string DOB { get; set; } = "";
+
+        [Required]
+        [StringLength(11)]
+        [RegularExpression(@"(^(0[7-9][0-1])\d{8}){1}")]
+        public string PhoneNumber { get; set; } = "";
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(8)]
+        [RegularExpression(@"[A-Z]+")]
+        public string DepartmentCode { get; set; } = "";
 	}
 
     public class EmployeeUpdateDto
     {
+        [MinLength(1)]
+        [MaxLength(20)]
+        [RegularExpression("[a-zA-Z-]+")]
+        [DefaultValue("John")]
         public string? FirstName { get; set; }
+
+        [MinLength(1)]
+        [MaxLength(20)]
+        [RegularExpression("[a-zA-Z-]+")]
+        [DefaultValue("Doe")]
         public string? LastName { get; set; }
+
+        [RegularExpression(@"^([1-3]?\d{1}\/[0-1]?\d{1}\/\d{4}){1}")]
+        [DefaultValue("16/10/2005")]
         public string? DOB { get; set; }
+
+        [StringLength(11)]
+        [RegularExpression(@"(^(0[7-9][0-1])\d{8}){1}")]
         public string? PhoneNumber { get; set; }
+
+        [MinLength(2)]
+        [MaxLength(8)]
+        [RegularExpression(@"[A-Z]+")]
         public string? DepartmentCode { get; set; }
     }
 }
